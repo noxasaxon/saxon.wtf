34b3bb4e43ab330c9ff8327791a65a9de041d3fc
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2022-07-02 14:53:12 -0700

    tash

---
+++ frontend/src/lib/components/Header.svelte
@@ -0,0 +1,18 @@
+<!-- Header.svelte -->
+<header>
+	<a href="/">Home</a>
+
+	<nav>
+		<ul>
+			<li>
+				<a href="/blog">Blog</a>
+			</li>
+			<li>
+				<a href="/about">About</a>
+			</li>
+			<!-- <li>
+				<a href="/contact">Contact</a>
+			</li> -->
+		</ul>
+	</nav>
+</header>
---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<!-- __layout.svelte -->
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>Hello, I'm the footer.</footer>
